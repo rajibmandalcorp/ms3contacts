5a000d4f7b1ada18cfc74b9a505de515e58a0809
rajibmandalcorp <51721651+rajibmandalcorp@users.noreply.github.com>
2019-12-03 18:11:26 -0600

    change host url

--- angular-js-application/config.srv.js
+++ angular-js-application/config.srv.js
@@ -7,8 +7,8 @@
 
     function configService($http, $q, $log) {
         var self = this;
-       // var host = "http://sys-api-mysql-ms3.us-e2.cloudhub.io"; cloud
-        //var host = "http://localhost:8081"; // local 
+        var host = "http://contacts-api-app.us-e2.cloudhub.io"; //cloud
+        //var host = "http://localhost:8081"; // local mule server
        
        self.host = host;
         self.GetContacts = host + "/api/contacts?offset=0&limit=10";
